2218122633ff19e6b31211669f130941ec2aba83
Paul Baibakov <paulc0@yandex.ru>
2020-05-21 03:00:39 +0300

    menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { NavBar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Nav, NavItem, NavDropdown} from 'react-bootstrap';
 import './CSS/Style.css';
 import {
 	Link
@@ -13,19 +13,23 @@ export default class MainMenu extends PureComponent {
 	render() {
 		return (
 			<div className="menuContainer">
-				<nav className="navbar navbar-expand-xl navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
+				<Nav className="navbar navbar-expand-xl navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
 					<div className="navbar-brand" style={{ width: "50%" }}><center> Логотип</center> </div>
-
-					<NavDropdown id="test1" title="test1">
-						<NavDropdown.Item ><Link to="/Test1_1"> test1_1</Link> </NavDropdown.Item>
+					<NavItem><button type="button" className="btn btn-outline-primary">btn</button> </NavItem>
+					<NavItem><button type="button" className="btn btn-outline-primary" ><Link to="/Test1_1"> test1_1</Link></button> </NavItem>
+					<NavDropdown id="test1" title="test1" >
+						<NavDropdown.Item><Link to="/Test1_1"> test1_1</Link> </NavDropdown.Item>
 						<NavDropdown.Item ><Link to="/Test1_2"> test1_2</Link> </NavDropdown.Item>
 					</NavDropdown>
+					
 					<NavItem><div className="menuItem"><Link to="/Test1_2"> test1_2</Link></div> </NavItem>
 					<NavItem><div className="menuItem"><Link to="/Test1_2"> test1_2</Link></div> </NavItem>
 					<NavItem><div className="menuItem"><Link to="/Test1_2"> test1_2</Link></div> </NavItem>
 					<NavItem><div className="menuItem"><Link to="/Test1_2"> test1_2</Link></div> </NavItem>
 					<NavItem><div className="menuItem"><Link to="/Test1_2"> test1_2</Link></div> </NavItem>
-				</nav>
+					<NavItem><button type="button" className="btn btn-outline-primary">btn</button> </NavItem>
+					<NavItem><button type="button" className="btn btn-outline-primary" ><Link to="/Test1_1"> test1_1</Link></button> </NavItem>
+				</Nav>
 			</div>
 		);
 	}
